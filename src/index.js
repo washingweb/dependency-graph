0f2e2188dc11efed13af2d216eea89eae6b15a59
DESKTOP-TUJOL2J\washwash <washingweb@hotmail.com>
2016-12-27 23:29:01 +0800

    改了一点点哦

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+const parse = (doc) => {
+  mappings = doc.split('\n').map(line => {
+    parts = line.split('->');
+    return [parts[0], parts[1]];
+  });
+
+  return mappings;
+}
+
 const app = new Vue({
   el: '#app',
+  computed : {
+    message : function() {
+      return JSON.stringify(parse(this.doc));
+    }
+  },
   data: {
     doc : ""
   }
