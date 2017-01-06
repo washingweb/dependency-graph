e04ebaa4442aea52100f71787b96c60e1e51e6e0
washwash <washingweb@hotmail.com>
2017-01-06 11:46:05 +0800

    更新样式

--- src/dot.js
+++ src/dot.js
@@ -2,6 +2,14 @@ function toDot(dependencies, properties) {
 
     const names = Object.keys(properties).filter(n => n != "");
     
+    const froms     = _.uniq(dependencies.map(d => d.from));
+    const tos       = _.uniq(dependencies.map(d => d.to));
+    const onlyFroms = _.difference(froms, tos);
+    const onlyTos   = _.difference(tos, froms);
+
+    const depNames  = _.uniq([].concat(froms).concat(tos));
+    const isolatedNames = _.difference(names, depNames);
+
     const objs = names.map(n => ({
         name  : n,
         props : properties[n],
@@ -12,24 +20,37 @@ function toDot(dependencies, properties) {
     });
 
     const groupNames = Object.keys(groups).filter(n => n != "");
-
     const rankGroups = groupNames.map(n => groups[n].map(o => o.name));
-
     const rankSpecs = rankGroups.map(arr => `{rank=same; ${arr.map(n => `"${n}"`).join(' ')}}`).join('\n');
 
     const personNames = [];
-    
+    const projectNames = [];
     names.forEach(n => {
         if (properties[n]["分类"] == "人") {
             personNames.push(n);
+        } else if (properties[n]["分类"] == "项目") {
+            projectNames.push(n);
         }
     });
 
+    const notPersonNames  = _.difference(onlyTos, personNames);
+    const notProjectNames = _.difference(onlyFroms, projectNames);
+
+    const errorNames = _.uniq(
+                            [].concat(notPersonNames)
+                              .concat(notProjectNames)
+                              .concat(isolatedNames));
+
+    // specs
+    const projectSpecs = projectNames.map(n => `"${n}" [shape=tab]`).join('\n');
+
     const personSpecs = personNames.map(n => `"${n}" [shape=circle]`).join('\n');
 
-    deps = dependencies.map(d => `"${d.from}" -> "${d.to}";`).join('\n');
+    const errorSpecs = errorNames.map(n => `"${n}" [color=red]`).join('\n');
 
     const nameDeclarations = names.map(n => `"${n}"`).join('\n');
+
+    const deps = dependencies.map(d => `"${d.from}" -> "${d.to}";`).join('\n');
 
     return `
 digraph {
@@ -42,7 +63,11 @@ ${rankSpecs}
 
 ${nameDeclarations}
 
+${projectSpecs}
+
 ${personSpecs}
+
+${errorSpecs}
 
 ${deps}
 }
